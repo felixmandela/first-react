39318557e199c74a45f164549087bf6788949c1a
Felix Mandela <felixmandela95@gmail.com>
2023-06-16 04:28:16 +0700

    test routing capabilities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/home';
+import About from './pages/about';
+import Navigation from './components/navigation';
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div>
+        <Navigation />
 
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
